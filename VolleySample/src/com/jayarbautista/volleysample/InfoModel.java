456881ea5d2571b6c0f84281130670f884a8f4f6
Jay Ar Bautista <j.bautista@klab.com>
2015-06-22 15:15:01 +0800

    Volley implementation

---
+++ VolleySample/src/com/jayarbautista/volleysample/InfoModel.java
@@ -0,0 +1,41 @@
+package com.jayarbautista.volleysample;
+
+
+public class InfoModel{
+    private String id;
+    private String name;
+    private String project;
+    private String quote;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getProject() {
+        return project;
+    }
+
+    public void setProject(String project) {
+        this.project = project;
+    }
+
+    public String getQuote() {
+        return quote;
+    }
+
+    public void setQuote(String quote) {
+        this.quote = quote;
+    }
+}
